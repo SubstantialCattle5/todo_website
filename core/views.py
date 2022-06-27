eec91a6fc27c78d448afbc2a224a6c8697c67738
samle <nilaynathsharan16@gmail.com>
2022-06-28 03:12:34 +0530

    adding the users features

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.core.checks import messages
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
@@ -18,11 +19,11 @@ def index(request):
             else:
                 new_task = Post.objects.create(
                     task=task,
-                    user='root'
+                    user=request.user.username
                 )
                 new_task.save()
         return render(request, 'index.html', {
-            'tasks': Post.objects.filter(user='root')
+            'tasks': Post.objects.filter(user=request.user.username)
         })
 
         # removing a task
@@ -56,3 +57,32 @@ def login(request):
 def logout(request):
     auth.logout(request)
     return redirect('login')
+
+
+# ------------------ Signup----------------------
+def signup(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password == confirm_password:
+            if User.objects.filter(username=username).exists():
+                messages.info(request, 'User already exist')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username,
+                                                password=password)
+                user.save()
+
+                # log user in and redirect to settings page
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
+
+                return redirect('index')
+
+        else:
+            messages.info(request, 'Password not Matching')
+            return redirect('signup')
+    else:
+        return render(request, 'signup.html')
